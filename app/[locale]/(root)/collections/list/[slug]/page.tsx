adb2b47f487dd8eb3c5dc82f0351b54dc13531b6
Truong Minh Tam <tamtm1@imt-soft.com>
2025-08-01 22:35:24 +0700

    fix not satisfy 5

--- app/[locale]/(root)/collections/list/[slug]/page.tsx
+++ app/[locale]/(root)/collections/list/[slug]/page.tsx
@@ -1,8 +1,16 @@
 import ProductList from '@/sections/collections/ProductList';
 import { getProductsByCategorySlug } from '@/lib/actions/product.action';
 
-export default async function CollectionsListPage({ params }: { params: { slug: string } }) {
-    const products = await getProductsByCategorySlug(params.slug);
+interface CollectionsListPageProps {
+    params: Promise<{
+        slug: string;
+    }>;
+}
+
+export default async function CollectionsListPage({ params }: CollectionsListPageProps) {
+    const { slug } = await params;
+
+    const products = await getProductsByCategorySlug(slug);
 
     return (
         <div className="pt-8 pb-16 mt-[140px]">
